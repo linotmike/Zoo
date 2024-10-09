55aff0e52cc7d5a1fde93d392bb0d5f871dd7111
linotmike <128604219+linotmike@users.noreply.github.com>
2024-10-08 21:45:40 -0700

    Added getters and setters and added Override toString() to improve object string representation

--- src/main/java/com/ps/Zoo.java
+++ src/main/java/com/ps/Zoo.java
@@ -17,4 +17,56 @@ public class Zoo {
         this.origin = origin;
         this.isPublic = isPublic;
     }
+
+    //Getters and Setters
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public boolean isPublic() {
+        return isPublic;
+    }
+
+    public void setPublic(boolean aPublic) {
+        isPublic = aPublic;
+    }
+
+    @Override
+    public String toString() {
+        return "Zoo{" +
+                "name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", age=" + age +
+                ", origin='" + origin + '\'' +
+                ", isPublic=" + isPublic +
+                '}';
+    }
 }
