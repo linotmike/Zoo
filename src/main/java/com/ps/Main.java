14d2249cd99d189d6426e6ef003f853c5e324493
linotmike <128604219+linotmike@users.noreply.github.com>
2024-10-08 22:02:41 -0700

    Added menu

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,7 +1,24 @@
 package com.ps;
+import java.util.Scanner;
 
 public class Main {
+        static Scanner scanner = new Scanner(System.in);
+        Zoo [] animals = new Zoo[10];
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int mainMenuCommand;
+
+        do{
+            System.out.println("Hello What would you like to do today?");
+            System.out.println("1) Add animal");
+            System.out.println("2) Display animal");
+            System.out.println("0) Exit");
+            mainMenuCommand = scanner.nextInt();
+
+
+        }while(mainMenuCommand != 0);
+
+
+
+
     }
 }
